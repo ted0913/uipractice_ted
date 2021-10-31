006e73df65b11f7610a7a61ca0fddddbc1b51979
TED <newlife913@naver.com>
2021-11-01 00:00:21 +0900

    Initial commit

---
+++ app/src/main/java/com/kgschem/uipractice_ted/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kgschem.uipractice_ted
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
